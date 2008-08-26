d14d02a8e886ebfae362dec1abd62d92f8895f0e
mattn <mattn.jp@gmail.com>
2008-08-26 07:40:23 +0000

    * 画面仕様変更に対応 * 絵文字だけのタイトルだと見えないのでtitleにはaltを使用

--- wassr-fav.py
+++ wassr-fav.py
@@ -3,15 +3,27 @@ import os
 import re
 import logging
 import wsgiref.handlers
-from BeautifulSoup import BeautifulSoup
+from BeautifulSoup import BeautifulSoup,Tag
 from google.appengine.api import urlfetch
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 
-def to_text(soup):
-  ret = ''.join([e for e in soup.recursiveChildGenerator() if isinstance(e,unicode)]).replace('&nbsp;', ' ')
-  soup = BeautifulSoup(ret, convertEntities=BeautifulSoup.HTML_ENTITIES)
-  return soup and soup.contents and soup.contents[0].encode('utf-8', 'replace').replace('\n', '') or ''
+def to_text(soup, use_img_alt=False):
+  ret = ""
+  for e in soup.recursiveChildGenerator():
+    if isinstance(e,Tag) and e.name == 'img' and not "icn-balloon" in e['src']:
+      if use_img_alt:
+        ret += "(%s)" % e['alt']
+      else:
+        e['src'] = "http://wassr.jp%s" % e['src']
+        ret += unicode(e)
+    elif isinstance(e,unicode):
+      ret += e.strip()
+  ret = ret.replace('&nbsp;', ' ').replace('  ', '\n')
+  #ret = ' '.join([e.strip() for e in soup.recursiveChildGenerator() if isinstance(e,unicode) or (isinstance(e,Tag) and e.name == 'img')]).replace('&nbsp;', ' ').replace('  ', '\n')
+  #soup = BeautifulSoup(ret, convertEntities=BeautifulSoup.HTML_ENTITIES)
+  #return soup and soup.contents and soup.contents[0].encode('utf-8', 'replace').replace('\n', '') or ''
+  return ret
 
 class MainPage(webapp.RequestHandler):
   def head(self):
@@ -36,10 +48,10 @@ class MainPage(webapp.RequestHandler):
       r1 = re.compile(r'.*/([^/]*)/$')
       r2 = re.compile(r'(.*)\([^\)]+\) (.*)')
       for page_fav in page_favs:
-        author = r1.sub(lambda x: x.group(1), page_fav.find('p', { 'class' : 'favorited_user noteTxt' }).find('a')['href'])
+        author = r1.sub(lambda x: x.group(1), page_fav.find('form', { 'class' : 'followbutton favorited_user noteTxt' }).find('a')['href'])
         #name = to_text(page_fav.find('p', { 'class' : 'favorited_user noteTxt' }).find('a'))
         template_values['favs'].append({
-          'title' : to_text(page_fav.find('p', { 'class' : 'message description' })).strip(),
+          'title' : to_text(page_fav.find('p', { 'class' : 'message description' }), True).strip(),
           'link' : 'http://wassr.jp%s' % page_fav.find('a', { 'class' : 'MsgDateTime' })['href'],
           'guid' : 'http://wassr.jp%s#%s' % (page_fav.find('a', { 'class' : 'MsgDateTime' })['href'], author),
           'author' : author,
